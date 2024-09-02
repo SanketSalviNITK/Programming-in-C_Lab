1c2f60872fad9a24a883d828cd91fbe3de434880
SanketSalviNITK <sanketsalvi.salvi@gmail.com>
2024-09-02 15:58:11 +0530

    Introduction to int, float and scanf

---
+++ Clab1b.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+    /*int x,y;
+    x=5;
+    y=2;
+    printf("This is X:%d",x);
+    printf("This is Y:%d",y);
+    printf("This is X+Y:%d",x+y);
+    printf("This is X-Y:%d",x-y);
+    printf("This is X*Y:%d",x*y);
+    printf("This is X/Y:%d",x/y);
+    */
+
+    /*float x,y;
+    x=5;
+    y=2;
+    printf("This is X:%f",x);
+    printf("This is Y:%f",y);
+    printf("This is X+Y:%f",x+y);
+    printf("This is X-Y:%f",x-y);
+    printf("This is X*Y:%f",x*y);
+    printf("This is X/Y:%f",x/y);
+    */
+
+   int x,y;
+   printf("Enter the number x:");
+   scanf("%f",&x);
+   printf("Enter the number y:");
+   scanf("%f",&y);
+   printf("This is X:%f",x);
+   printf("This is Y:%f",y);
+   printf("This is X+Y:%f",x+y);
+   printf("This is X-Y:%f",x-y);
+   printf("This is X*Y:%f",x*y);
+   printf("This is X/Y:%f",x/y);
+
+ return 0;
+}

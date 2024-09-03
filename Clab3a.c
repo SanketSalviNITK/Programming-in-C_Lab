807f139261a70532bae218122ae039ae362689a3
SanketSalviNITK <sanketsalvi.salvi@gmail.com>
2024-09-03 09:55:21 +0530

    Intro to stdlib and time headers

---
+++ Clab3a.c
@@ -0,0 +1,13 @@
+//C program to understand rand() and srand() function for genrating a random number
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main()
+{
+    int randomNum;
+    srand(time(0)); 
+
+    randomNum=(rand() % 100) + 1;
+    printf("%d",randomNum);
+}

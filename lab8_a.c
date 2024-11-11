b85bdd14977538499040fe200bbf16015b9559e8
SanketSalviNITK <39799838+SanketSalviNITK@users.noreply.github.com>
2024-11-11 10:15:55 +0530

    Added new function

--- lab8_a.c
+++ lab8_a.c
@@ -2,15 +2,19 @@
 int get_slength(char*);
 int get_charnum(char*);
 int get_vownum(char*);
+void string_copy(char*,char*);
 
 int main(){
     char str[100];
+    char str2[100];
     printf("Enter a string:");
     gets(str);
     printf("Echo:%s",str);
     printf("\nThe string length is:%d",get_slength(str));
     printf("\nThe number of character in string is:%d",get_charnum(str));
     printf("\nThe number of vowels in string is:%d",get_vownum(str));
+    string_copy(str,str2);
+    printf("\nThe copied string is:%s",str2);
     return 0;
 }
 
@@ -45,4 +49,13 @@ int get_vownum(char* p){
         p++;
     }
     return num;
+}
+
+void string_copy(char* s1,char* s2){
+    while(*s1!='\0'){
+        *s2=*s1;
+        s1++;
+        s2++;
+    }
+    *s2='\0';
 }
